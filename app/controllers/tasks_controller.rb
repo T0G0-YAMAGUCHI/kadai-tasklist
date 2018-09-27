bcadcdded801e404435a39ab3967ac186b658041
kumiko-sakai <ec2-user@ip-172-31-87-116.ec2.internal>
2018-09-27 10:53:00 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  
+  before_action :set_task, only: [:show, :update, :destroy, :edit]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -23,11 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] ="タスクが更新されました"
@@ -39,7 +39,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success]= "タスクが削除されました"
@@ -47,6 +46,12 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
     params.require(:task).permit(:content)
   end
