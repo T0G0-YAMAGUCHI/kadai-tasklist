240d2bc80c363bcf292b3c2fcefd09da5f22de36
kumiko-sakai <ec2-user@ip-172-31-87-116.ec2.internal>
2018-10-07 07:11:43 +0000

    make task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,48 +1,36 @@
 class TasksController < ApplicationController
-  
+  before_action :require_user_logged_in
+  before_action :correct_user , only: [:destroy]
   before_action :set_task, only: [:show, :update, :destroy, :edit]
   
-  def index
-    @tasks = Task.all.page(params[:page]).per(10)
-  end
   
-  def show
-  end
+
   
+
   def new
     @task = Task.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクが正常に追加されました"
-     redirect_to @task
+     redirect_to root_url
     else
       flash[:danger]="タスクの追加に失敗しました"
       render :new
     end
   end
   
-  def edit
-  end
+
   
-  def update
-    
-    if @task.update(task_params)
-      flash[:success] ="タスクが更新されました"
-      redirect_to @task
-    else
-      flash[:danger] = "タスクが変更されませんでした"
-      render :edit
-    end
-  end
+
   
   def destroy
     @task.destroy
     
     flash[:success]= "タスクが削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -54,5 +42,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content , :status)
+  end
+  
+  def correct_user
+    @task= current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
